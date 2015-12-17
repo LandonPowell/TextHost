8639af5d1d1a14aa486a1acebdfc8c79d5545a63
Landon Powell <landonpowell@linuxmail.org>
2015-12-17 00:21:14 -0500

    Minor fix because L^MP is an annoying retard and get is using my code as a meme now.
    Well, I guess the whole 'minimalism' thing isn't going to happen since people are going to be retarded and use my non-production code. Fucking jesus.

--- textFiles/index.php
+++ textFiles/index.php
@@ -2,6 +2,7 @@
     $fileList = scandir("./");
     foreach ($fileList as $file) {
         if ( pathinfo($file)['extension'] == "txt" ) {
+            $file = htmlspecialchars($file);
             echo "<a href=\"$file\"/> $file </a><br>";  
         }
     }
