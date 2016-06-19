840f5617cb8ca61bc1668ded5fa2dfd636afb3da
CubaHub <Pandya967@gmail.com>
2016-06-19 11:37:16 -0400

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,5 +1,10 @@
 <?php
     $fileURL = "textFiles/" . $_POST["fileName"] . ".txt";
+    if(!file_exists($fileURL)){
     file_put_contents($fileURL, $_POST["fileContent"]);
-    header("location:" . $fileURL);                         
+    header("location:" . $fileURL);                    
+    }
+    else{
+        die("A paste with that name already exists! pick a different name");
+    }
 ?>
